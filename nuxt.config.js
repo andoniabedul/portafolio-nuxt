5051f07c109c67e4d66f310d721158a8c6f39fad
Andoni Abedul <andoniabedul@gmail.com>
2021-05-04 21:04:59 +0200

    Client app

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ const contentfulConfig = require('./.contentful.json');
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-
+  ssr: false,
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Andoni Abedul',
